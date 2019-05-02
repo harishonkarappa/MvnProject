c0fbb84c385980a7f660321f25d5ecccd8792a96
Harish Onkarappa <onkarappa.harish@gmail.com>
2019-05-02 03:51:39 -0700

    commit

---
+++ src/main/java/com/mkyong/hashing/App.java
@@ -0,0 +1,26 @@
+package com.mkyong.hashing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+public boolean isPrime(int num){
+	boolean val = true;
+	if(num == 1 || num == 2)
+		return false;
+	for (int i = 2; i < num/2; i++){
+		if(num%i == 0) {
+		val = false;
+		break;
+		}	
+	}
+return val;
+}
+}
